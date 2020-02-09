e5ba756e575940a750d5fdaf16b658dcd2f7fb84
Jerma066 <Jerma066@users.noreply.github.com>
2020-02-09 14:40:19 +0300

    Реализация функций ошибок MSE и MAE.

---
+++ Lave_Maker/dependances/errors.cpp
@@ -0,0 +1,29 @@
+﻿#include "errors.h"
+
+//MSE
+float MSE(std::function<float (float)> dependance, QMap<float, float> values)
+{
+    float error = 0;
+    int n = values.size();
+
+    //pair.key() = x; pair.value() = y
+    for(auto pair = values.begin(); pair != values.end(); pair++){
+        error += pow((dependance(pair.key()) - pair.value()), 2);
+    }
+    error /= n;
+    return error/n;
+}
+
+//MAE
+float MAE(std::function<float (float)> dependance, QMap<float, float> values)
+{
+    float error = 0;
+    int n = values.size();
+
+    //pair.key() = x; pair.value() = y
+    for(auto pair = values.begin(); pair != values.end(); pair++){
+        error += abs((dependance(pair.key()) - pair.value()));
+    }
+    error /= n;
+    return error/n;
+}
