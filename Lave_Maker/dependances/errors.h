124974ff427cc5ba4e3f4fa1c024fc7eb8a16feb
Jerma066 <Jerma066@users.noreply.github.com>
2020-03-08 18:44:07 +0300

    Подключение библиотеки cmath.

--- Lave_Maker/dependances/errors.h
+++ Lave_Maker/dependances/errors.h
@@ -3,6 +3,7 @@
 
 #include <functional>
 #include <QMap>
+#include <cmath>
 
 float MSE(std::function<float(float x)>, QMap<float, float>);
 float MAE(std::function<float(float x)>, QMap<float, float>);
