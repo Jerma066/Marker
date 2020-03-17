1715edb1de2aaf1de3bc5aadb0f92230f015376c
Jerma066 <Jerma066@users.noreply.github.com>
2020-03-17 12:11:28 +0300

    Незначительные изменнеия в mainwindow. Добавление пункта в файл фронт работ.

--- Lave_Maker/mainwindow.cpp
+++ Lave_Maker/mainwindow.cpp
@@ -33,11 +33,9 @@ void MainWindow::on_actionOpen_triggered()
     QFileInfo file_info(path);
     QString suffix = file_info.suffix();
 
-    std::shared_ptr<FileReader> fr = ReadFile(path, suffix);
+    std::shared_ptr<FileReader> fr = ReadFile(path, suffix, Orientation::Vertical);
 
-    std::vector<std::vector<float>> test = {{1, 2, 3},
-                                            {4, 5, 6},
-                                            {7, 8 ,9}};
+    std::vector<std::vector<float>> test = fr->data();
 
     //Блок вызова диалогового окна
     dataTableDialog dt(test, this);
@@ -46,4 +44,5 @@ void MainWindow::on_actionOpen_triggered()
         dataTable data = dt.getDataTable();
         //TODO: дальнейшая обработка данных.
     }
+
 }
